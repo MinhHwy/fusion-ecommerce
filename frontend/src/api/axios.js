98b44df5afdb8e6d87bdf830ad861d4d4c5bee6d
MinhHwy <huyuwu55@gmail.com>
2026-01-16 00:41:28 +0700

    Fix frontend structure and Vercel build

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: "https://fusion-ecommerce.onrender.com",
+  withCredentials: true
 });
 
 export default api;
+
