8cb54a854104d5e1c24b925436455bd43bddb2ea
MinhHwy <huyuwu55@gmail.com>
2026-01-17 11:50:07 +0700

    add users/me route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -61,6 +61,19 @@ router.get('/id/:id', async (req, res) => {
     res.status(500).json({ error: 'Server error' });
   }
 });
+// ✅ GET USER FROM TOKEN
+router.get("/me", protect, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    if (!user) {
+      return res.status(404).json({ msg: "User not found" });
+    }
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ msg: "Server error" });
+  }
+});
 
 
 module.exports = router;
